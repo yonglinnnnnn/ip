de21799614e5bf0f2fb197f7d950f3be2a33e5f3
yonglin <yongglinx@gmail.com>
2025-08-18 14:20:52 +0800

    Fix bugs in echo message logic

--- src/main/java/MegaBot.java
+++ src/main/java/MegaBot.java
@@ -1,22 +1,24 @@
+import java.util.Scanner;
+
 public class MegaBot {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
                 "Hello! I'm MegaBot\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n\n" +
-        "list\n" +
-        "       ____________________________________________________________\n" +
-        "        list\n" +
-        "       ____________________________________________________________\n\n"+
-        "blah\n" +
-        "       ____________________________________________________________\n" +
-        "        blah\n" +
-        "       ____________________________________________________________\n\n" +
-        "bye\n" +
-        "       ____________________________________________________________\n" +
-        "        Bye. Hope to see you again soon!\n" +
-        "       ____________________________________________________________\n";
-
+                "____________________________________________________________\n\n";
         System.out.println(intro);
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (!userInput.equals("bye")) {
+            userInput = scanner.nextLine(); // Reads a String
+            System.out.print("     ____________________________________________________________\n");
+
+            System.out.println("     " + userInput);
+            System.out.print("     ____________________________________________________________\n");
+        }
+
+        scanner.close();
+
     }
 }
