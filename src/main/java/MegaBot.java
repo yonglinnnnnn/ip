6ac8aaf1b5a4e9844c95b6b606f38a4d53b9c4fb
yonglin <yongglinx@gmail.com>
2025-08-18 13:57:26 +0800

    Add greeting

--- src/main/java/MegaBot.java
+++ src/main/java/MegaBot.java
@@ -1,10 +1,12 @@
 public class MegaBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "____________________________________________________________\n" +
+                "Hello! I'm MegaBot\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(intro);
     }
 }
