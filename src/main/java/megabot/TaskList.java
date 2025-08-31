a6f8669ac0ac8596f73a3881d87ebc7f02f4c912
yonglin <yongglinx@gmail.com>
2025-08-31 18:18:20 +0800

    Add JavaDoc for MegaBot and TaskList

--- src/main/java/megabot/TaskList.java
+++ src/main/java/megabot/TaskList.java
@@ -5,33 +5,64 @@ import megabot.task.Task;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a collection of tasks with operations to manage them.
+ * Provides methods to add, delete, mark, and retrieve tasks.
+ *
+ * @author Xu Yong Lin
+ * @version 1.0
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
-
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with the given list of tasks.
+     *
+     * @param tasks the initial list of tasks
+     */
     public TaskList(ArrayList<Task> tasks) {
 
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task the task to be added
+     */
     public void addTask(Task task) {
 
         tasks.add(task);
     }
 
+    /**
+     * Deletes a task at the specified index.
+     *
+     * @param index the index of the task to delete (0-based)
+     * @throws InvalidTaskException if the index is out of bounds
+     */
     public void deleteTask(int index) throws InvalidTaskException {
         if (index < 0 || index > tasks.size()) {
             throw new InvalidTaskException("Please give a valid number to delete the task from!!");
         }
-        if (index >= 0 && index < tasks.size()) {
+        if (index < tasks.size()) {
             tasks.remove(index);
         }
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param index the index of the task to retrieve (0-based)
+     * @return the task at the specified index, or null if index is invalid
+     */
     public Task getTask(int index) {
         if (index >= 0 && index < tasks.size()) {
             return tasks.get(index);
@@ -39,6 +70,11 @@ public class TaskList {
         return null;
     }
 
+    /**
+     * Marks the task at the specified index as done.
+     *
+     * @param index the index of the task to mark (0-based)
+     */
     public void markTask(int index) {
         Task task = getTask(index);
         if (task != null) {
@@ -46,6 +82,11 @@ public class TaskList {
         }
     }
 
+    /**
+     * Marks the task at the specified index as not done.
+     *
+     * @param index the index of the task to unmark (0-based)
+     */
     public void unmarkTask(int index) {
         Task task = getTask(index);
         if (task != null) {
@@ -53,20 +94,41 @@ public class TaskList {
         }
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return the size of the task list
+     */
     public int size() {
 
         return tasks.size();
     }
 
+    /**
+     * Checks if the task list is empty.
+     *
+     * @return true if the task list is empty, false otherwise
+     */
     public boolean isEmpty() {
 
         return tasks.isEmpty();
     }
 
+    /**
+     * Returns the underlying ArrayList of tasks.
+     *
+     * @return the list of tasks
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Checks if the given index is valid for the current task list.
+     *
+     * @param index the index to check
+     * @return true if the index is valid, false otherwise
+     */
     public boolean isValidIndex(int index) {
         return index >= 0 && index < tasks.size();
     }
